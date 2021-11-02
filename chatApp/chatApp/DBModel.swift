06a00cb44a85506a801f0cf6bdaa405fd9a6e8df
Sana Alshahrani <me-sa51@hotmail.com>
2021-11-02 11:10:55 +0300

    Sana Alshahrani

---
+++ chatApp/chatApp/DBModel.swift
@@ -0,0 +1,35 @@
+//
+//  DBModel.swift
+//  chatApp
+//
+//  Created by Sana Alshahrani on 24/03/1443 AH.
+//
+
+import Foundation
+import MessageKit
+
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
+}
+
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
+
+
+struct ChatAppMessage{
+    let fullName: String
+    let message: String
+}
+
+
+struct User{
+    let fullName: String
+    let id = UUID()
+    let isOnline: String
+}
+
