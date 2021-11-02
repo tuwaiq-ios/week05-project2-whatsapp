1e2f60b6ebbda559865fbb5985e936b6c03d44cb
Jawaher <jawaher.hass@gmail.com>
2021-11-02 19:09:22 +0300

    done

---
+++ ChatAp/ChatAp/ProfileViewModel.swift
@@ -0,0 +1,13 @@
+
+
+import Foundation
+
+enum ProfileViewModelType {
+    case info, logout
+}
+
+struct ProfileViewModel {
+    let viewModelType: ProfileViewModelType
+    let title: String
+    let handler: (() -> Void)?
+}
